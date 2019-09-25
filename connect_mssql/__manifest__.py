0bb26520b352708886174c6baa6a5a0f22c6c4c4
ehabmosilhy <ehabmosilhy@gmail.com>
2019-09-25 20:06:34 +0300

    First Commit

---
+++ connect_mssql/__manifest__.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+{
+      'name': 'Connect to MS SQL Server Database'
+    , 'version': '1.0.0'
+    , 'category': 'Tools'
+    , 'author': "Ehab Mosilhy"
+    , 'depends': ['base']
+    , 'data': ['views/connect_mssql.xml']
+}
