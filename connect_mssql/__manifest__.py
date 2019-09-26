f1ebe06144df89c7da58a12eaa6d0335842b3f97
ehabmosilhy <ehabmosilhy@gmail.com>
2019-09-26 11:43:02 +0300

    Added license key and an image

--- connect_mssql/__manifest__.py
+++ connect_mssql/__manifest__.py
@@ -1,9 +1,11 @@
 # -*- coding: utf-8 -*-
 {
-      'name': 'Connect to MS SQL Server Database'
+    'name': 'Connect to MS SQL Server Database'
     , 'version': '1.0.0'
     , 'category': 'Tools'
     , 'author': "Ehab Mosilhy"
+    , 'license': 'LGPL-3'
     , 'depends': ['base']
     , 'data': ['views/connect_mssql.xml']
+    , 'images': ['static/description/connect_mssql.png'],
 }
